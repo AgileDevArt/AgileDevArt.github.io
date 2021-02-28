54cf1982e678f54b3bf55f2b83d030856abc9683
Marko Vranicar <agiledevart@gmail.com>
2021-02-28 23:30:01 +0100

    initial commit

---
+++ MonoSingleton.cs
@@ -0,0 +1,26 @@
+﻿namespace UnityEngine
+{
+    /// <summary>
+    /// This class creates it's own instance if not assigned to an existing gameobject
+    /// </summary>
+    public abstract class MonoSingleton<tt> : MonoBehaviour where tt : Component
+    {
+        static tt _instance;
+        public static tt Instance => _instance ?? SetInstance(new GameObject(typeof(tt).Name).AddComponent<tt>());
+
+        static tt SetInstance(tt instance)
+        {
+            if (_instance != instance)
+                DontDestroyOnLoad(_instance = instance);
+            return _instance;
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+                SetInstance(this as tt ?? throw new System.InvalidCastException(typeof(tt).Name));
+            if (_instance != this)
+                Destroy(this.gameObject);
+        }
+    }
+}
